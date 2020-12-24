b1bb6145428493ceac0158ced10310b90167cf09
Mukesh Murugan <iammukeshm@gmail.com>
2020-12-24 22:11:02 +0530

    added brand validator / fluent

---
+++ AspNetCoreHero.Boilerplate.Web/Areas/Catalog/Validators/BrandViewModelValidator.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreHero.Boilerplate.Web.Areas.Catalog.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreHero.Boilerplate.Web.Areas.Catalog.Validators
+{
+    public class BrandViewModelValidator : AbstractValidator<BrandViewModel>
+    {
+
+        public BrandViewModelValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+
+            RuleFor(p => p.Tax).GreaterThanOrEqualTo(1).WithMessage("{PropertyName} must be greater than 1");
+
+        }
+    }
+}
