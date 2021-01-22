d5890728adac21cce30d408a5339d5ea557626b4
yurekliisa <yurekliisa@gmail.com>
2021-01-22 18:12:19 +0300

    added log pipeline

---
+++ AspNetCoreHero.Boilerplate.Application/Pipelines/LoggingBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetCoreHero.Boilerplate.Application.Pipelines
+{
+    public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public LoggingBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogInformation("Request: {Name}", requestName);
+                return await next();
+            }
+            catch (Exception ex)
+            {                
+                throw ex;
+            }
+        }
+    }
+}
