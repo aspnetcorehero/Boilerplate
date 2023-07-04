b3a8790cc1a453aba33c7a704ecbd95a6da7abb1
Prasanth A <prabhu.a24@gmail.com>
2023-07-04 18:00:32 +0530

    running mode

---
+++ AspNetCoreHero.Boilerplate.Infrastructure/Migrations/ApplicationDb/20230704113325_initial2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCoreHero.Boilerplate.Infrastructure.Migrations.ApplicationDb
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
